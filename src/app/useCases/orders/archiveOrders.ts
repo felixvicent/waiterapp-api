844e0b40361197db25d04f5d1b8d4555d965266f
Félix Vicente <felixvicent1306@gmail.com>
2023-01-02 16:28:06 -0300

    Emit event on archive orders

--- src/app/useCases/orders/archiveOrders.ts
+++ src/app/useCases/orders/archiveOrders.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
+import { io } from "../../..";
 
 import { Order } from "../../models/Order";
 
 export async function archiveOrders(req: Request, res: Response) {
   try {
     await Order.updateMany({}, { $set: { archive: true } });
+
+    io.emit("orders@archive", true);
 
     return res.sendStatus(204);
   } catch (error) {
